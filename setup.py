2bb22cad77983d79bcccb6303352ade2b16f85ac
Konstantin Trushin <konstantin.trushin@gmail.com>
2022-02-19 00:43:08 +0300

    Change quoting style

--- setup.py
+++ setup.py
@@ -8,26 +8,26 @@ import setuptools
 def read_long_description() -> str:
     """Returns the content of README.md file."""
     cwd = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(cwd, "README.md")) as f:
+    with open(os.path.join(cwd, 'README.md')) as f:
         return f.read()
 
 setuptools.setup(
-    name="pynaive",
-    version="0.2.0",
-    description="A toy project to demonstrate " \
-                "Python development environment and toolchain",
+    name='pynaive',
+    version='0.2.0',
+    description='A toy project to demonstrate ' \
+                'Python development environment and toolchain',
     long_description=read_long_description(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/ktrushin/pynaive",
-    author="Konstantin Trushin",
-    author_email="konstantin.trushin@gmail.com",
-    license="MIT",
+    long_description_content_type='text/markdown',
+    url='https://github.com/ktrushin/pynaive',
+    author='Konstantin Trushin',
+    author_email='konstantin.trushin@gmail.com',
+    license='MIT',
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
     ],
-    packages=["naive"],
+    packages=['naive'],
     include_package_data=True,
     install_requires=[],
     entry_points={}
